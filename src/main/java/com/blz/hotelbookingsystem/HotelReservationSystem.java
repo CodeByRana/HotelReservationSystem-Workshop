eadd1702548caa3dd6391f09c3f82d8e0d91b924
deepakphpweb <dipakrana2760@gmail.com>
2021-12-22 08:28:28 +0530

    [Deepak Added ] : Welcome to Hotel Reservation System

---
+++ src/main/java/com/blz/hotelbookingsystem/HotelReservationSystem.java
@@ -0,0 +1,7 @@
+package com.blz.hotelbookingsystem;
+
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
